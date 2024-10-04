94c06b74d935bdaf90fbf1421acc95fb0a938aba
Pietroro98 <pietroro98@gmail.com>
2024-10-04 15:41:20 +0200

    aggiungo metodo che stampai dettagli dei giochi forniti dettagli gioco

--- src/main/java/PietroRomano/Application.java
+++ src/main/java/PietroRomano/Application.java
@@ -1,42 +1,25 @@
 package PietroRomano;
 
-import java.sql.SQLOutput;
+
+
 
 public class Application {
 
     public static void main(String[] args) {
 
-        Collezione collezione = new Collezione();
-        System.out.println("--------------------------------------------------------------");
-        Videogioco videogioco1 = new Videogioco(1,"FIFA 25",2024, 59.88, "PS5", 7, Genere.SPORT);
-        Videogioco videogioco2 = new Videogioco(2,"Assassin's Creed Shadows | Limited Edition", 2025, 79.99, "XBOX Series X", 1800 /*1800=30 ore in minuti*/, Genere.AZIONE);
-        Videogioco videogioco3 = new Videogioco(3,"Guitar Hero 5",2010, 69.99, "PC", 1200, Genere.MUSICA);
-
-        System.out.println("VIDEOGIOCO:  ");
-        System.out.println("ID: " + videogioco1.getIdGioco());
-        System.out.println("Titolo: " + videogioco1.getTitolo());
-        System.out.println("Anno di pubblicazione: " + videogioco1.getAnnoPubblicazioneGioco());
-        System.out.println("Durata media: " + videogioco1.getDurataInMinuti());
-        System.out.println("Prezzo : " + videogioco1.getPrezzo() + " €");
-        System.out.println("Genere: " + videogioco1.getGenere());
 
         System.out.println("--------------------------------------------------------------");
+
+        Videogioco videogioco1 = new Videogioco(1, "FIFA 25", 2024, 59.88, "PS5", 7, Genere.SPORT);
+        Videogioco videogioco2 = new Videogioco(2, "Assassin's Creed Shadows | Limited Edition", 2025, 79.99, "XBOX Series X", 1800, Genere.AZIONE);
+        Videogioco videogioco3 = new Videogioco(3, "Guitar Hero 5", 2010, 69.99, "PC", 1200, Genere.MUSICA);
 
         GiocoDaTavolo giocoDaTavolo1 = new GiocoDaTavolo(4, "Monopoly Ultimate Banking", 2016, 82.37, 4, 20);
         GiocoDaTavolo giocoDaTavolo2 = new GiocoDaTavolo(5, "RISIKO", 2017, 35.79, 6, 480);
         GiocoDaTavolo giocoDaTavolo3 = new GiocoDaTavolo(6, "Gioco dell'oca", 2023, 7.99, 6, 50);
 
-        System.out.println("GIOCO DA TAVOLO:");
-        System.out.println("ID: " + giocoDaTavolo1.getIdGioco());
-        System.out.println("Titolo: " + giocoDaTavolo1.getTitolo());
-        System.out.println("Anno Pubblicazione: " + giocoDaTavolo1.getAnnoPubblicazioneGioco());
-        System.out.println("Prezzo: " + giocoDaTavolo1.getPrezzo() + " €");
-        System.out.println("Numero di Giocatori: " + giocoDaTavolo1.getNumeroGiocatori());
-        System.out.println("Durata Media Partita: " + giocoDaTavolo1.getDurataInMinuti());
 
-        System.out.println("--------------------------------------------------------------");
-
-        System.out.println("Aggiungi gioco alla collezione:");
+        Collezione collezione = new Collezione();
         collezione.aggiungiElemento(videogioco1);
         collezione.aggiungiElemento(videogioco2);
         collezione.aggiungiElemento(videogioco3);
@@ -45,19 +28,35 @@ public class Application {
         collezione.aggiungiElemento(giocoDaTavolo3);
 
 
+        System.out.println("VIDEOGIOCHI: ");
+        stampaDettagliGioco(videogioco1);
+        stampaDettagliGioco(videogioco2);
+        stampaDettagliGioco(videogioco3);
+
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("GIOCHI DA TAVOLO:");
+        stampaDettagliGioco(giocoDaTavolo1);
+        stampaDettagliGioco(giocoDaTavolo2);
+        stampaDettagliGioco(giocoDaTavolo3);
+
         System.out.println("--------------------------------------------------------------");
 
-        System.out.println("Ricerca Gioco per ID: ");
-        Gioco gioco = collezione.ricercaGiocoPerId(1);
-        if (gioco != null) {
-            System.out.println("Gioco trovato per ID 1: " + gioco.getTitolo());
+    }
+    private static void stampaDettagliGioco (Gioco gioco){
+        System.out.println("ID: " + gioco.getIdGioco());
+        System.out.println("Titolo: " + gioco.getTitolo());
+        System.out.println("Anno di pubblicazione: " + gioco.getAnnoPubblicazioneGioco());
+        System.out.println("Prezzo : " + gioco.getPrezzo() + " €");
+
+        if (gioco instanceof Videogioco videogioco) {
+            System.out.println("Piattaforma: " + videogioco.getPiattaforma());
+            System.out.println("Durata media: " + videogioco.getDurataInMinuti());
+            System.out.println("Genere: " + videogioco.getGenere());
+        } else {
+            GiocoDaTavolo g = (GiocoDaTavolo) gioco;
+            System.out.println("Numero di Giocatori: " + g.getNumeroGiocatori());
+            System.out.println("Durata Media Partita: " + g.getDurataInMinuti());
         }
-
         System.out.println("--------------------------------------------------------------");
-        System.out.println("Ricerca Gioco per prezzo: ");
-
-
-
-
     }
 }
--- src/main/java/PietroRomano/Collezione.java
+++ src/main/java/PietroRomano/Collezione.java
@@ -1,8 +1,6 @@
 package PietroRomano;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Collezione {
@@ -21,14 +19,54 @@ public class Collezione {
 
     public Gioco ricercaGiocoPerId(int id) {
         Gioco gioco = this.collezione.get(id);
-        if (gioco == null) {
-            System.out.println("! Nessun gioco trovato con questo id !");
-            return null;
+        if (!collezione.containsKey(id)) {
+            System.out.println("! Nessun gioco trovato con l'ID: " + id);
         }
-        return gioco;
+        return collezione.get(id);
     }
 
-    public List<Gioco> ricarcaGiocoPerPrezzo (double prezzoMax) {
+    public List<Gioco> ricercaGiocoPerPrezzo (double prezzoMax) {
         return collezione.values().stream().filter(gioco -> gioco.getPrezzo() <= prezzoMax).collect(Collectors.toList());
+    }
+
+    public List<GiocoDaTavolo> ricercaPerNumeroGiocatori(int numeroGiocatori) {
+        return collezione.values().stream().filter(gioco -> gioco instanceof GiocoDaTavolo)
+                .map(gioco ->(GiocoDaTavolo)gioco).filter(gioco -> gioco.getNumeroGiocatori() <= numeroGiocatori)
+                .collect(Collectors.toList()) ;
+    }
+
+    public void rimuoviElemento(int id) {
+        if (!collezione.containsKey(id)) {
+            System.out.println("! Nessun gioco puó essere rimosso con questo id !");
+            return;
+        }
+        collezione.remove(id);
+    }
+
+    public void aggiornaElemento(Gioco gioco) {
+        if (!collezione.containsKey(gioco.getIdGioco())) {
+            System.out.println("Errore: Nessun gioco trovato con l'ID specificato.");
+            return;
+        }
+        collezione.put(gioco.getIdGioco(), gioco);
+    }
+
+    public void stampaStatistiche() {
+        long numeroVideogiochi = collezione.values().stream().filter(gioco -> gioco instanceof Videogioco).count();
+        long numeroGiochiDaTavolo = collezione.values().stream().filter(gioco -> gioco instanceof GiocoDaTavolo).count();
+        OptionalDouble mediaPrezzi = collezione.values().stream().mapToDouble(Gioco::getPrezzo).average();
+        Optional<Gioco> giocoPrezzoMax = collezione.values().stream().max(Comparator.comparingDouble(Gioco::getPrezzo));
+
+        System.out.println("Numero totale di videogiochi: " + numeroVideogiochi);
+        System.out.println("Numero totale di giochi da tavolo: " + numeroGiochiDaTavolo);
+
+        if (giocoPrezzoMax.isPresent()) {
+            Gioco giocoPrezzoMassimo = giocoPrezzoMax.get();
+            System.out.println("Gioco con il prezzo piú alto: " + giocoPrezzoMassimo.getTitolo() +
+                    " - " + giocoPrezzoMassimo.getPrezzo() + " €");
+        } else {
+            System.out.println("Media dei prezzi di tutti i giocatori: " + mediaPrezzi + " €");
+        }
+
     }
 }
